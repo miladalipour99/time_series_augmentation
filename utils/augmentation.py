e85c9f9c63d3c52e331b53b1b086a610a0213eba
miladalipour99 <60439091+miladalipour99@users.noreply.github.com>
2022-01-10 11:41:35 -0800

    Update augmentation.py

--- utils/augmentation.py
+++ utils/augmentation.py
@@ -1,11 +1,11 @@
 import numpy as np
 from tqdm import tqdm
 
-def jitter(x, sigma=0.03):
+def jitter(x, sigma=3):
     # https://arxiv.org/pdf/1706.00527.pdf
     return x + np.random.normal(loc=0., scale=sigma, size=x.shape)
 
-def scaling(x, sigma=0.1):
+def scaling(x, sigma=1):
     # https://arxiv.org/pdf/1706.00527.pdf
     factor = np.random.normal(loc=1., scale=sigma, size=(x.shape[0],x.shape[2]))
     return np.multiply(x, factor[:,np.newaxis,:])
