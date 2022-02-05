a4583ab586fe0d38ca83011cba2797ea12570e39
miladalipour99 <60439091+miladalipour99@users.noreply.github.com>
2022-02-05 12:22:51 +0330

    Update augmentation.py

--- utils/augmentation.py
+++ utils/augmentation.py
@@ -1,11 +1,11 @@
 import numpy as np
 from tqdm import tqdm
 
-def jitter(x, sigma=0.9):
+def jitter(x, sigma=0.03):
     # https://arxiv.org/pdf/1706.00527.pdf
     return x + np.random.normal(loc=0., scale=sigma, size=x.shape)
 
-def scaling(x, sigma=1.2):
+def scaling(x, sigma=0.1):
     # https://arxiv.org/pdf/1706.00527.pdf
     factor = np.random.normal(loc=1., scale=sigma, size=(x.shape[0],x.shape[2]))
     return np.multiply(x, factor[:,np.newaxis,:])
@@ -36,7 +36,7 @@ def permutation(x, max_segments=5, seg_mode="equal"):
             ret[i] = pat
     return ret
 
-def magnitude_warp(x, sigma=2, knot=4):
+def magnitude_warp(x, sigma=0.2, knot=4):
     from scipy.interpolate import CubicSpline
     orig_steps = np.arange(x.shape[1])
     
